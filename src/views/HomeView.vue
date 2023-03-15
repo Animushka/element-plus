32fd382d66154346de64aac24aaabe57dca43c8a
grigory <fawer2990@gmail.com>
2023-03-15 20:35:33 +0300

    add

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <el-button @click="getWeather">Get Weather</el-button>
+    <el-card v-if="weatherData">{{ weatherData }}</el-card>
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+<script lang="ts" setup>
+import { ref } from 'vue'
+import axios from 'axios'
 
-@Options({
-  components: {
-    HelloWorld
-  }
+navigator.geolocation.getCurrentPosition(function (position) {
+  geo(position.coords.latitude, position.coords.longitude)
 })
-export default class HomeView extends Vue {}
+let latitude: number
+let longitude: number
+
+const geo = (lat : number, lon: number) => {
+  latitude = lat
+  longitude = lon
+}
+
+const url = 'https://api.weather.yandex.ru/v2/informers?'
+
+const weatherData = ref()
+
+const getWeather = () => {
+  axios.get(url, {
+    headers: {
+      'X-Yandex-API-Key': 'da49491d-cacf-4ecf-a756-284b4b7fa430\n'
+    },
+    params: {
+      lat: latitude,
+      lon: longitude
+    }
+  })
+    .then(res => {
+      weatherData.value = res.data
+    })
+    .catch(err => console.log(err))
+}
+
 </script>
+
+<style>
+.flex-grow {
+  flex-grow: 1;
+}
+</style>
